528636db6a8f0bb8605632e671ea0ff88032e845
Julian <julian.tabordab1@gmail.com>
2023-06-19 17:09:26 -0500

    update css and js 2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
-document.addEventListener('DOMContentLoaded', function () {
-    var burgerMenuToggle = document.querySelector('.burger-menu-toggle');
-    var burgerMenu = document.querySelector('.burger-menu');
-    var crossMenu = document.querySelector('.cross-menu');
+let burgerMenuToggle = document.querySelector('.burger-menu-toggle');
+let burgerMenu = document.querySelector('.burger-menu');
+let crossMenu = document.querySelector('.cross-menu');
   
     burgerMenuToggle.addEventListener('click', function () {
       burgerMenu.style.left = '0';
@@ -12,6 +9,3 @@ document.addEventListener('DOMContentLoaded', function () {
     crossMenu.addEventListener('click', function () {
       burgerMenu.style.left = '100%';
     });
-  });
-  
->>>>>>> f4007350a4266321a880bc5a3d3846765bceac91
