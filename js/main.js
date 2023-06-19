f4007350a4266321a880bc5a3d3846765bceac91
Julian <julian.tabordab1@gmail.com>
2023-06-19 16:33:14 -0500

    Update js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const crossIcon = document.querySelector('cross-menu');
-const sectionsLinks = document.querySelectorAll('mobile-menu-item');
-const burgerImg = document.querySelector('burger-menu-img');
-// function toggleMenu() {
-//     burgerImg.classList.toggle('burger-menu-toggle');
-// }
-// urgerMenu.addEventListener('click', toggleMenu);
-
-
-
-//  openMenu.addEventListener('click',show);
-//  closeMenu.addEventListener('click',close);
-
-//  function show(){
-//      mainMenu.style.display = 'flex';
-//      mainMenu.style.top = '0';
-//  }
-//  function close(){
-//      mainMenu.style.top = '-100%';
-//  }
+document.addEventListener('DOMContentLoaded', function () {
+    var burgerMenuToggle = document.querySelector('.burger-menu-toggle');
+    var burgerMenu = document.querySelector('.burger-menu');
+    var crossMenu = document.querySelector('.cross-menu');
+  
+    burgerMenuToggle.addEventListener('click', function () {
+      burgerMenu.style.left = '0';
+    });
+  
+    crossMenu.addEventListener('click', function () {
+      burgerMenu.style.left = '100%';
+    });
+  });
+  
