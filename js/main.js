baaa2232153b07ce8c52d5e00e54755680e70bc7
Rebelzob <karimdavid07@gmail.com>
2023-06-20 11:10:22 +0100

    Added JS logic to burger menu, fixed some styles related to the burger menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-let burgerMenuToggle = document.querySelector('.burger-menu-toggle');
-let burgerMenu = document.querySelector('.burger-menu');
-let crossMenu = document.querySelector('.cross-menu');
-  
-    burgerMenuToggle.addEventListener('click', function () {
-      burgerMenu.style.left = '0';
-    });
-  
-    crossMenu.addEventListener('click', function () {
-      burgerMenu.style.left = '100%';
-    });
+const burgerMenuToggle = document.querySelector('.burger-menu-toggle');
+const burgerMenu = document.querySelector('.burger-menu');
+const crossMenu = document.querySelector('.cross-menu');
+const mobileItems = document.querySelectorAll('.mobile-menu-item');
+
+// toggle burger menu
+burgerMenuToggle.addEventListener('click', () => {
+  burgerMenu.classList.remove('burger-menu-hidden');
+});
+
+// hide burger menu
+crossMenu.addEventListener('click', () => {
+  burgerMenu.classList.add('burger-menu-hidden');
+});
+
+// links to sections
+mobileItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    const targetId = item.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      burgerMenu.classList.add('burger-menu-hidden');
+    }
+  });
+});
