2d08b9f3c39d19c618c4711ee0d126afd68ca0f5
Rebelzob <karimdavid07@gmail.com>
2023-06-23 13:21:44 +0100

    Added email form validator

---
+++ js/validators.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form');
+const emailInput = document.getElementById('email');
+const errorMsg = document.querySelector('span.error');
+
+form.addEventListener('submit', (e) => {
+  const email = emailInput.value;
+  const lowercaseEmail = email.toLowerCase();
+
+  if (email !== lowercaseEmail) {
+    errorMsg.textContent = 'please use lowercase only';
+    errorMsg.style.backgroundColor = '#ef863a';
+    errorMsg.classList.add('active');
+    e.preventDefault();
+  } else {
+    errorMsg.textContent = 'Form successfully submitted';
+    errorMsg.style.backgroundColor = '#4caf50';
+    errorMsg.classList.add('active');
+    e.preventDefault();
+  }
+});
